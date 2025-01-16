3a7c67a865f3ccb38af01efe720331fb902b9c19
apipapip <hafiefmuhammad@gmail.com>
2025-01-16 10:22:38 +0700

    CRUD

--- crud/index.php
+++ crud/index.php
@@ -7,24 +7,57 @@
     <link rel="stylesheet" href="dist/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="row mt-5">
-        <div class="col-12">
-            <div class="container">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
                 <a href="view/siswa/tambah.php" class="btn btn-lg btn-success">Tambah Data</a>
             </div>
         </div>
-    </div>
-    <?php
+
+        <div class="row mt-5">
+            <div class="col-12">
+                
+                <h1>Data Siswa</h1>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>NISN</th>
+                            <th>Kelas</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
     include_once "class/Siswa.php";
 
     $siswa=new siswa();
     $data_all=$siswa->tampil_data_siswa_all();
-
+    $no=1;
     foreach($data_all as $da)
     {
-        echo "<li>$da[nama]-$da[nisn]-$da[kelas]</li>";
+        echo'<tr>
+        <td>'.$no.'</td>
+        <td>'.$da["nama"].'</td>
+        <td>'.$da["nisn"].'</td>
+        <td>'.$da["kelas"].'</td>
+        <td>
+        <a href="controller/siswaController.php?aksi&id='.$da["id"].'"
+        class="btn btn-sm btn-danger">Hapus</a>
+        
+        <a href="view/edit.php?id='.$da["id"].'" class="btn btn-sm btn-warning">Edit</a>
+        </td>
+        </tr>';
+        $no++;
     }
 ?>
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>
 <script src ="dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
